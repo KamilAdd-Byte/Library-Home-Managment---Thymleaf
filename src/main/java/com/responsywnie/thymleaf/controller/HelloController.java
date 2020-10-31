8d6b90ecfadf7aed072d8c87c5f77a8405c21cb8
KamilAdd-Byte <kamillodzinski09@gmail.com>
2020-10-31 20:30:28 +0100

    Work in new book.html. Add action th:action th:object and POST method

--- src/main/java/com/responsywnie/thymleaf/controller/HelloController.java
+++ src/main/java/com/responsywnie/thymleaf/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.responsywnie.thymleaf.controller;
 
+import com.responsywnie.thymleaf.model.Book;
 import com.responsywnie.thymleaf.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,5 +17,11 @@ public class HelloController {
         public String get (Model model){
         model.addAttribute("listBooks",bookService.getAllBooks());
         return "helloo";
+    }
+    @GetMapping("/showNewBookForm")
+    public String showNewBookForm(Model model){
+        Book book = new Book();
+        model.addAttribute("book",book);
+        return "new book";
     }
 }
